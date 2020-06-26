fefc4c595b8f507feb675829f4b79138b1a8476e
unknown <brw1820@gmail.com>
2020-06-25 20:34:21 -0400

    changes to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,104 +1,16 @@
-const inquirer = require("inquirer");
-const fs = require('fs');
-// const { EXDEV } = require("constants");
-const generateMarkdown = require("./utils/generateMarkdown")
-
 // array of questions for user
 const questions = [
-    // questions to user using "inquirer"
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
-    },
 
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title"
-    },
-
-    {
-        type: "input",
-        message: "What is your GitHub repository?",
-        name: "repo"
-    },
-
-    {
-        type: "input",
-        message: "Please provide a short description of your project here:",
-        name: "desc"
-    },
-
-    {
-        type: "input",
-        message: "Please provide instructions for properly installing your application here:",
-        name: "install"
-    },
-
-    {
-        type: "input",
-        message: "Please provide instructions on how to use your application here:",
-        name: "usage"
-    },
-
-    {
-        type: "input",
-        message: "Were there any contributors on your project?",
-        name: "contribution"
-
-    },
-    {
-        type: "list",
-        message: "Which license are you using?",
-        name: "License:",
-        choices: [
-          "MIT", 
-          "ISC", 
-          "Apache", 
-        ]},
-
-    {
-        type: "input",
-        message: "How would someone test your application?",
-        name: "test"
-    }
-
-        ];
-
-// const response = [
-
-// ];
-
-
-function init() {
-    inquirer
-        .prompt(questions)
-        .then(function(answers) {
-            console.log(answers);
-
-            // var filename = answers.title.toLowerCase().split(' ').join('') + ".json";
-          
-            fs.writeFile("README.md", generateMarkdown(answers), function(err) {
-          
-              if (err) {
-                return console.log(err);
-              }
-            });
-        });
-    };
-              console.log("Success!");
+];
 
 // function to write README file
-// fs.appendFile("README.md", generateMarkdown(answers), function (err) {
-//     if (err) {
-//         throw err;
-//     }})
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
 
 // function call to initialize program
 init();
-
-
-module.exports = {
-    inti:init,
-};
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,7 @@
 // function to generate markdown for README
-function generateMarkdown(answers) {
-  return `# ${answers.title}
-  ## ${answers.desc}
-  ## Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contributing](#contributing)
-  * [Test](#test)
-  * [Questions](#questions)
-  * [License](#license)
-  * [Badges](#badges)
-  ## Installation Instructions
-  ${answers.install}
-  ## Usage
-  ${answers.usage}
-  ## Contributors
-  ${answers.contribution}
-  ## Test
-  ${answers.test}
-  ## Questions
-  If you have any questions, please feel free to contact ${answers.username} on GitHub.
-  
+function generateMarkdown(data) {
+  return `# ${data.title}
+
 `;
 }
 
